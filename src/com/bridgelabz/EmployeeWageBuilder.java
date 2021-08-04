a8d7beb3e61f4a95516ac31b9de7a669f368db5c
SUDIPP22 <panja370@gmail.com>
2021-08-05 01:04:52 +0530

    Refactored : Employee Wage Calc Using While Condition

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -7,16 +7,20 @@ public class EmployeeWageBuilder {
         //constant
         int WAGE_PER_HOUR = 20;
         int NUM_OF_WORKING_DAYS = 20;
+        int MAX_HRS_IN_MONTH = 100;
         int IS_FULL_TIME = 1;
         int IS_PART_TIME = 2;
 
         //variables
-        int dailyWage = 0;
+        int totalEmpHrs = 0;
         int empHrs = 0;
         int totalEmpWage = 0;
+        int totalWorkingDays = 0;
 
         //calculation
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+
             int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (empcheck) {
@@ -33,10 +37,10 @@ public class EmployeeWageBuilder {
                     System.out.println("Employee is Absent ");  //Absent
             }
 
-            dailyWage = empHrs * WAGE_PER_HOUR;
-            totalEmpWage += dailyWage;
-            System.out.println("Salary of Day : " + (day + 1) + " is " + dailyWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours ");
         }
-        System.out.println("Total Wages of a Month is : " + totalEmpWage);
+        totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
+        System.out.println("Total Wages is : " + totalEmpWage);
     }
 }
