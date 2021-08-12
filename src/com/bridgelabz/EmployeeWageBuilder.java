ce026a21c4a4969cd89bdc59a01c91f6eb7fdb2e
SUDIPP22 <panja370@gmail.com>
2021-08-12 13:20:29 +0530

    Refactored : Computation of Employee Wage for Multiple Companies

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,49 +1,66 @@
 package com.bridgelabz;
 
+/**
+ * Purpose : Computation of Employee Wage for
+ * Multiple Companies in a procedural way
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-08-12
+ */
 public class EmployeeWageBuilder {
-    //constant
-    public static final int WAGE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HRS_IN_MONTH = 100;
-    public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
 
-    public static int computeEmployeeWage() {
+    /**
+     * This method is used to calculate Employee wage for
+     * different companies in a procedural way.
+     * @param company This is the first parameter of computeEmployeeWage method
+     * which represents company name
+     * @param empWagePerHour This is the second parameter of computeEmployeeWage method
+     * which consists employee rate per hour
+     * @param numOfWorkingDays This is the third parameter of computeEmployeeWage method
+     * which consists the number of working days of an employee
+     * @param maxHoursInMonth This is the fourth parameter of computeEmployeeWage method
+     * which hold the maximum hours the employee will be working
+     * @return Employee's total wage
+     */
+    public static int computeEmployeeWage(String company, int empWagePerHour,
+                                          int numOfWorkingDays, int maxHoursInMonth) {
         //variables
         int totalEmpHrs = 0;
         int empHrs;
         int totalWorkingDays = 0;
 
         //calculation
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= maxHoursInMonth && totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
 
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (empCheck) {
-                case 1:
+                case 1:                                  /* Employee Works as a Full-time Employee */
                     empHrs = 8;
-                    System.out.println("\nEmployee is present and working as a Full-Time employee");  //Full time
                     break;
-                case 2:
+                case 2:                                 /* Employee Works as a Part-time Employee */
                     empHrs = 4;
-                    System.out.println("\nEmployee is present and working as a Part-Time employee ");  // Part-time
                     break;
-                default:
+                default:                               /* Employee is absent */
                     empHrs = 0;
-                    System.out.println("\nEmployee is Absent ");  //Absent
             }
 
             totalEmpHrs += empHrs;
-            System.out.println("Day " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours ");
+            System.out.println("\nDay " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours ");
         }
-        int totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
-        System.out.println("\nTotal Wage is : " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * empWagePerHour;
+        System.out.println("\nTotal Wage for Company " + company + " is : " + totalEmpWage);
         return totalEmpWage;
     }
 
+    /**
+     * This is the main method which makes use of computeEmployeeWage method
+     * @param args Unused.
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        computeEmployeeWage();
+        computeEmployeeWage("BIG BAZAAR", 20, 20, 80);
+        computeEmployeeWage("D-MART", 40, 30, 100);
     }
 }
